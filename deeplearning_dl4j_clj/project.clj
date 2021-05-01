49b9ef88c23a4f0e5d250535f7da4f6bf479f7ed
Mark Watson <markw@markwatson.com>
2021-05-01 07:48:56 -0700

    problem: no M1 arm64 ND4J backend

--- deeplearning_dl4j_clj/project.clj
+++ deeplearning_dl4j_clj/project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot deeplearning-dl4j-clj.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.deeplearning4j/deeplearning4j-datasets "1.0.0-beta7"]
+                 [org.deeplearning4j/deeplearning4j-core "1.0.0-beta7"]
+                 [org.nd4j/nd4j-native "1.0.0-beta7"]]
+  :main ^:skip-aot deeplearning-dl4j-clj.wisconsin_data
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- deeplearning_dl4j_clj/src/deeplearning_dl4j_clj/wisconsin_data.clj
+++ deeplearning_dl4j_clj/src/deeplearning_dl4j_clj/wisconsin_data.clj
@@ -1,9 +1,84 @@
-(ns deeplearning-dl4j-clj.wisconsin-data)
+(ns deeplearning-dl4j-clj.wisconsin-data
+  (:import [org.datavec.api.split FileSplit]
+           [org.datavec.api.util ClassPathResource]
+           [org.datavec.api.io.labels ParentPathLabelGenerator]
+           [org.datavec.image.recordreader ImageRecordReader]
+           [org.nd4j.linalg.dataset.api.preprocessor ImagePreProcessingScaler]
+           [org.datavec.image.loader NativeImageLoader]
+           [org.deeplearning4j.datasets.datavec
+            RecordReaderDataSetIterator
+            SequenceRecordReaderDataSetIterator RecordReaderDataSetIterator$Builder]
+           [org.datavec.api.records.reader.impl.csv
+            CSVRecordReader
+            CSVSequenceRecordReader]
+           [org.deeplearning4j.nn.conf
+            NeuralNetConfiguration$Builder
+            GradientNormalization
+            ]
+           [org.deeplearning4j.nn.api OptimizationAlgorithm]
+           [org.deeplearning4j.nn.weights WeightInit]
+           [org.nd4j.linalg.activations Activation]
+           [org.nd4j.linalg.lossfunctions LossFunctions$LossFunction]
+           [org.deeplearning4j.optimize.listeners ScoreIterationListener]
+           [org.deeplearning4j.nn.multilayer MultiLayerNetwork]
+           [org.deeplearning4j.util ModelSerializer]
+           [org.deeplearning4j.nn.conf
+            BackpropType
+            WorkspaceMode]
+           [org.deeplearning4j.nn.conf.layers
+            SubsamplingLayer$Builder
+            SubsamplingLayer$PoolingType
+            ConvolutionLayer$Builder
+            RnnOutputLayer$Builder
+            GravesLSTM$Builder
+            LSTM$Builder
+            DropoutLayer$Builder
+            OutputLayer$Builder
+            DenseLayer$Builder
+            LocalResponseNormalization$Builder GravesBidirectionalLSTM$Builder]
+           [org.deeplearning4j.nn.conf.inputs InputType]
+           [org.deeplearning4j.nn.conf.distribution
+            NormalDistribution
+            GaussianDistribution]
+           [java.io File]
+           [java.util Random]
+           [org.nd4j.linalg.schedule StepSchedule MapSchedule ScheduleType ExponentialSchedule InverseSchedule PolySchedule SigmoidSchedule]
+           [org.deeplearning4j.nn.conf.layers.variational VariationalAutoencoder VariationalAutoencoder$Builder]
+           [org.nd4j.linalg.learning.config Nesterovs Adam RmsProp Sgd AdaDelta AdaGrad AdaMax Nadam NoOp]))
+
 ;; https://github.com/hswick/jutsu.ai/blob/master/src/jutsu/ai/core.clj
+
+(def numHidden 3)
+(def numOutputs 1)
+(def batchSize 64)
+
+(def initial-seed (long 33117))
+
+(def numInputs 9)
+(def labelIndex 9)
+(def numClasses 2)
 
 
 (defn -main
   "Using DL4J with Wisconsin data"
   [& args]
-  (println "Hello, World!"))
+  (let [recordReader (new CSVRecordReader)
+        _ (. recordReader initialize (new FileSplit (new File "data/","training.csv")))
+        trainIter (new RecordReaderDataSetIterator recordReader batchSize labelIndex numClasses)
+        recordReaderTest (new CSVRecordReader)
+        _ (. recordReaderTest initialize (new FileSplit (new File "data/","testing.csv")))
+        testIter (new RecordReaderDataSetIterator recordReaderTest batchSize labelIndex numClasses)
+        conf (->
+               (new NeuralNetConfiguration$Builder)
+               (.seed initial-seed)
+               (.activation Activation/TANH)
+               (.weightInit( WeightInit/XAVIER))
+        ;;TBD finish above on Intel Mac (no ARM DL4J backend
+        ]
+    ;;(. conf  (activation Activation/TANH))
+    ;;(-> conf  (.activation Activation/TANH))
+    (println trainIter)
+    (println testIter)
+    (println conf)
+    ))
 
