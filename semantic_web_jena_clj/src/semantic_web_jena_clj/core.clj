6b7c10fcd89b76f36bb68b88b801297d4cf92b7b
Mark Watson <markw@markwatson.com>
2021-05-01 06:27:26 -0700

    done with first cut at Jena examples

--- semantic_web_jena_clj/src/semantic_web_jena_clj/core.clj
+++ semantic_web_jena_clj/src/semantic_web_jena_clj/core.clj
@@ -6,8 +6,22 @@
 
 (defonce model (get-jena-api-model))
 
+(defn- results->clj [results]
+  (let [variable-list (seq (. results variableList))
+        bindings-list (seq (map seq (. results rows)))]
+    (cons variable-list bindings-list)))
+
 (defn load-rdf-file [fpath]
   (. model loadRdfFile fpath))
 
 (defn query "SPARQL query" [sparql-query]
-  (. model query sparql-query))
+  (results->clj (. model query sparql-query)))
+
+(defn query-remote "remote service like DBedia, etc." [remote-service sparql-query]
+  (results->clj (. model queryRemote remote-service sparql-query)))
+
+(defn query-dbpedia [sparql-query]
+  (query-remote "https://dbpedia.org/sparql" sparql-query))
+
+(defn query-wikidata [sparql-query]
+  (query-remote "\"https://query.wikidata.org/bigdata/namespace/wdq/sparql" sparql-query))
--- semantic_web_jena_clj/test/semantic_web_jena_clj/core_test.clj
+++ semantic_web_jena_clj/test/semantic_web_jena_clj/core_test.clj
@@ -5,8 +5,18 @@
 (deftest load-data-and-sample-queries
   (testing "Load local triples files and make some SPARQL queries"
     (load-rdf-file "data/sample_news.nt")
-    (let [results (query "select * { ?s ?p ?o } limit 5")
-          variable-list (seq (. results variableList))]
-      (println results)
-      (println variable-list))
+    (let [results (query "select * { ?s ?p ?o } limit 5")]
+      (println results))
     (is (= 0 0))))
+
+(deftest dbpedia-test
+  (testing "Try SPARQL query to DBPedia endpoint"
+    (println
+      (query-dbpedia
+        "select ?p where { <http://dbpedia.org/resource/Bill_Gates> ?p <http://dbpedia.org/resource/Microsoft> . } limit 10"))))
+
+(deftest wikidata-test
+  (testing "Try SPARQL query to WikiData endpoint"
+    (println
+      (query-dbpedia
+        "select * where { ?subject ?property ?object . } limit 10"))))
