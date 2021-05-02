8d7c12ab38358538ecf9aa1f2dd137d05b620c9c
Mark Watson <markw@markwatson.com>
2021-05-02 09:21:29 -0700

    -

--- python_interop_deeplearning/project.clj
+++ python_interop_deeplearning/project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :jvm-opts ["-Djdk.attach.allowAttachSelf"
+             "-XX:+UnlockDiagnosticVMOptions"
+             "-XX:+DebugNonSafepoints"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  ;;[clj-python/libpython-clj "1.37"]
                  [clj-python/libpython-clj "2.00-beta-15"]
