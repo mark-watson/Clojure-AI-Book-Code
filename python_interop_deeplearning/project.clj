91dfd6f93f96561f545c174b56f423284ab0ef40
Mark Watson <markw@markwatson.com>
2021-05-02 10:05:45 -0700

    -

--- python_interop_deeplearning/project.clj
+++ python_interop_deeplearning/project.clj
@@ -8,7 +8,7 @@
              "-XX:+DebugNonSafepoints"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  ;;[clj-python/libpython-clj "1.37"]
-                 [clj-python/libpython-clj "2.00-beta-15"]
+                 [clj-python/libpython-clj "1.37"]
                  ]
   :main ^:skip-aot python-interop-deeplearning.core
   :target-path "target/%s"
