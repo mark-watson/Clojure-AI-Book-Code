92ee836f9889660b38692d94dbc4e41a7d7d5ebf
Mark Watson <markw@markwatson.com>
2021-05-03 20:33:41 +0000

    trying to get transformers working

--- python_interop_deeplearning/src/python_interop_deeplearning/core.clj
+++ python_interop_deeplearning/src/python_interop_deeplearning/core.clj
@@ -5,6 +5,9 @@
 (require-python '[numpy :as np])
 (require-python '[nltk :as nltk])
 (require-python '[nltk.book :as book])
+(require-python '[transformers :as tr])
+
+;;(def qa (tr/pipeline "question-answering" :model "NeuML/bert-small-cord19-squad2" :tokenizer "NeuML/bert-small-cord19qa"))
 
 (defn foo1 []
   (np/array [[1 2][3 4]]))
@@ -12,7 +15,12 @@
 (defn foo2 []
   (nltk/download "book")
   (println book/texts))
-  
+
+;;(defun foo3 []
+;;  (println (qa { "question" "Where does Bill Gates live?"
+;;                 "context"  "Bill and Melinda Gate's home is in Seatle"
+;;	       })))
+	       
 (defn -main
   [& args]
   (println "These examples run in the repl. Don't 'lein run', rather 'lein repl'"))
