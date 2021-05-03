dc66d95d264e85686e5860759d4a194ae4a802c9
Mark Watson <markw@markwatson.com>
2021-05-03 20:07:08 +0000

    fixing simple examples

--- python_interop_deeplearning/src/python_interop_deeplearning/core.clj
+++ python_interop_deeplearning/src/python_interop_deeplearning/core.clj
@@ -2,11 +2,18 @@
     (:require [libpython-clj.require :refer [require-python]]
               [libpython-clj.python :as py :refer [py. py.. py.-]]))
 
+(require-python '[numpy :as np])
+(require-python '[nltk :as nltk])
+(require-python '[nltk.book :as book])
+
+(defn foo1 []
+  (np/array [[1 2][3 4]]))
+
+(defn foo2 []
+  (nltk/download "book")
+  (println book/texts))
+  
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  ;;(py/initialize! :python-executable "/Users/markw/bin/conda/bin/python"
-  ;;                :library-path "/Users/markw/bin/conda/pkgs/python-3.8.8-h4e93d89_0_cpython/lib/libpython3.8.dylib")
-  (require-python '[numpy :as np])
-  (def test-ary (np/array [[1 2][3 4]]))
-  (println test-ary))
+  (println "These examples run in the repl. Don't 'lein run', rather 'lein repl'"))
+  
