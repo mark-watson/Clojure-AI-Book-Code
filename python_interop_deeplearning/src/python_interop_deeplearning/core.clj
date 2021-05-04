c65dfae3e99a57fe924820ad44e54f0320d54cf6
Mark Watson <markw@markwatson.com>
2021-05-04 20:15:02 +0000

    spacy experiments

--- python_interop_deeplearning/src/python_interop_deeplearning/core.clj
+++ python_interop_deeplearning/src/python_interop_deeplearning/core.clj
@@ -5,9 +5,24 @@
 (require-python '[numpy :as np])
 (require-python '[nltk :as nltk])
 (require-python '[nltk.book :as book])
-(require-python '[transformers :as tr])
+;;(require-python '[transformers :as tr])
+(require-python '[spacy :as sp])
 
 ;;(def qa (tr/pipeline "question-answering" :model "NeuML/bert-small-cord19-squad2" :tokenizer "NeuML/bert-small-cord19qa"))
+
+;;nlp = spacy.load("en_core_web_sm")
+;;doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+
+(def nlp (sp/load "en_core_web_sm"))
+(def doc2 (nlp  "John Smith went to Mexico"))
+(println doc2) 
+(for [token doc2]
+  (println token))
+(let [doc (nlp "Apple is looking at buying U.K. startup for $1 billion")]
+  (map (fn [token]
+         [(py.- token text) (py.- token pos_) (py.- token dep_)])
+       doc))
+
 
 (defn foo1 []
   (np/array [[1 2][3 4]]))
