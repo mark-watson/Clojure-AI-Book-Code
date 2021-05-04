c6192ee9be2972bfc8d5ca02cde145b3e147f3b8
“Mark <markw@markwatson.com>
2021-05-04 22:05:48 +0000

    spacy NLP

--- python_interop_deeplearning/src/python_interop_deeplearning/core.clj
+++ python_interop_deeplearning/src/python_interop_deeplearning/core.clj
@@ -5,7 +5,6 @@
 (require-python '[numpy :as np])
 (require-python '[nltk :as nltk])
 (require-python '[nltk.book :as book])
-;;(require-python '[transformers :as tr])
 (require-python '[spacy :as sp])
 
 ;;(def qa (tr/pipeline "question-answering" :model "NeuML/bert-small-cord19-squad2" :tokenizer "NeuML/bert-small-cord19qa"))
@@ -14,15 +13,35 @@
 ;;doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
 (def nlp (sp/load "en_core_web_sm"))
-(def doc2 (nlp  "John Smith went to Mexico"))
-(println doc2) 
-(for [token doc2]
+
+(def test-text "John Smith worked for IBM in Mexico last year and earned $1 million in salary and bonuses.")
+(def doc2 (nlp test-text))
+(println doc2)
+(doseq [token doc2]
   (println token))
 (let [doc (nlp "Apple is looking at buying U.K. startup for $1 billion")]
   (map (fn [token]
          [(py.- token text) (py.- token pos_) (py.- token dep_)])
        doc))
-
+(defn text->tokens [text]
+  (map (fn [token] (py.- token text))
+       (nlp text)))
+;;  (text->tokens test-text)
+(defn text->pos [text]
+  (map (fn [token] (py.- token pos_))
+       (nlp text)))
+;;  (text->pos test-text)
+  
+(defn text->tokens-and-pos [text]
+  (map (fn [token] [(py.- token text) (py.- token pos_)])
+       (nlp text)))
+;;  (text->tokens-and-pos test-text)
+;;for ent in doc.ents:
+;;    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+(defn text->entities [text]
+  (map (fn [entity] (py.- entity label_))
+       (py.- (nlp text) ents)))
+;;  (text->entities test-text)
 
 (defn foo1 []
   (np/array [[1 2][3 4]]))
