826583262be5f8c11f0a072cf73be386c4b904f0
Mark Watson <markw@markwatson.com>
2021-05-07 08:21:52 -0700

    work on caching

--- knowledge_graph_navigator/src/knowledge_graph_navigator_clj/cache.clj
+++ knowledge_graph_navigator/src/knowledge_graph_navigator_clj/cache.clj
@@ -2,6 +2,8 @@
   (:require [knowledge-graph-navigator-clj.sparql :as sparql])
   (:use clojure.pprint))
 
+(require '[cemerick.url :refer (url-encode url-decode)])
+
 (require '[next.jdbc :as jdbc])
 (require '[next.jdbc.result-set :as rs])
 
@@ -15,11 +17,14 @@
 
 (defn read-cache [a-query]
   (create-table-if-not-exists)
-  (let [results (jdbc/execute-one! ds [(str "select * from DBPEDIA where query='" a-query "'")]
-                                   {:builder-fn rs/as-unqualified-lower-maps})]
-    (let [r (get results :result nil)]
-      (let [ret2 (if r (read-string (str "[" r "]")) nil)]
-        ret2))))
+  (let [s1 (url-encode (pr-str a-query))
+        results (jdbc/execute-one! ds [(str "select * from DBPEDIA where query='" s1 "'")]
+                                   {:builder-fn rs/as-unqualified-lower-maps})
+        data (get results :result nil)]
+    ;;(pprint results)
+    (pprint data)
+    (pprint (url-decode data))
+    (if data (read-string (url-decode data) nil))))
 
 (defn cached-sparql-query [a-query]
   (let [cached-result (read-cache a-query)]
@@ -27,8 +32,10 @@
       cached-result
       (do
         (let [result (sparql/dbpedia a-query)
+              s1 (url-encode  (pr-str a-query))
+              s2 (url-encode (pr-str result))
               sql-insert (clojure.string/replace
-                           (str "insert into DBPEDIA(query,result) values('" a-query "','" result "')")
+                           (str "insert into DBPEDIA(query,result) values('" s1 "','" s2 "')")
                            ;;(str "insert into DBPEDIA(query,result) values('" a-query "','" (apply str result) "')")
                             #"\"\"" "\"")]
                             ;;#"\"\"" "\"")]
@@ -36,10 +43,18 @@
           (println "Writing data to cache...")
           result)))))
 
-(comment
-  (def test-query "select * { ?s ?p ?o } limit 3")
+(defn -main [& args]
+  (def test-query "select * { ?s ?p ?o } limit 5")
 
   (def z (cached-sparql-query test-query))
   (println "z" z)
+  (println "(type z)" (type z))
   (println "- first:" (first z))
-  (println "- second:" (second z)))
+  (println "- second:" (second z))
+
+  (def z2 (cached-sparql-query test-query))
+  (println "z2" z2)
+  (println "(type z2)" (type z2))
+  (println "- first:" (first z2))
+  (println "- second:" (second z2)))
+
