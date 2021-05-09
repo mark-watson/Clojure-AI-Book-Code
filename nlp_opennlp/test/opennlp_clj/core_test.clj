5360f9764b3175c3e4553ceb65dfb819f6190f9f
Mark Watson <markw@markwatson.com>
2021-05-09 15:15:07 -0700

    -

--- nlp_opennlp/test/opennlp_clj/core_test.clj
+++ nlp_opennlp/test/opennlp_clj/core_test.clj
@@ -1,19 +1,19 @@
 (ns opennlp-clj.core-test
-  (:require [clojure.test :refer :all])
-  (:require [opennlp-clj.core :refer :all]))
+  (:require [clojure.test :as test])
+  (:require [opennlp-clj.core :as onlp]))
 
 (def
   test-text
   "The cat chased the mouse around the tree while Mary Smith (who works at IBM in San Francisco) watched.")
 
-(deftest pos-test
-  (testing "parts of speech"
-    (let [token-java-array (tokenize->java test-text)
-          token-clojure-seq (tokenize->seq test-text)
-          words-pos (POS token-java-array)
-          companies (company-names token-java-array)
-          places (location-names token-java-array)
-          people (person-names token-java-array)]
+(test/deftest pos-test
+  (test/testing "parts of speech"
+    (let [token-java-array (onlp/tokenize->java test-text)
+          token-clojure-seq (onlp/tokenize->seq test-text)
+          words-pos (onlp/POS token-java-array)
+          companies (onlp/company-names token-java-array)
+          places (onlp/location-names token-java-array)
+          people (onlp/person-names token-java-array)]
       (println "Input text:\n" test-text)
       (println "Tokens as Java array:\n" token-java-array)
       (println "Tokens as Clojure seq:\n" token-clojure-seq)
@@ -21,4 +21,4 @@
       (println "Companies:\n" companies)
       (println "Places:\n" places)
       (println "People:\n" people)
-      (is (= (first words-pos) "DT")))))
+      (test/is (= (first words-pos) "DT")))))
