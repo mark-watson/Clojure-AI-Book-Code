5a1bc288e7555f0d738a33b234ae9dff3d91847d
Mark Watson <markw@markwatson.com>
2021-05-21 10:17:58 -0700

    removed experimentel code

--- core_logic_examples/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to core_logic_eamples_clj
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- core_logic_examples/test/core_logic_eamples_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns core-logic-eamples-clj.core-test
-  (:require [clojure.test :refer :all]
-            [core-logic-eamples-clj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
