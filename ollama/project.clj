161cb61d2ee66bd541496970e1a952aa9cac6cec
Mark Watson <markw@markwatson.com>
2024-02-03 13:08:47 -0700

    example using local LLMs run using Ollama REST service

---
+++ ollama/project.clj
@@ -0,0 +1,10 @@
+(defproject ollama_api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.3.1"]
+                 ]
+  :repl-options {:init-ns ollama-api.core})
