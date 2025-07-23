ea2b722d602adf9e69457698efce691c87fc6e44
Mark Watson <markw@markwatson.com>
2025-07-23 10:28:51 -0700

    -

--- semantic_web_jena/test/semantic_web_jena_clj/core_test.clj
+++ semantic_web_jena/test/semantic_web_jena_clj/core_test.clj
@@ -5,6 +5,7 @@
 
 (deftest load-data-and-sample-queries
   (testing "Load local triples files and make some SPARQL queries"
+    (println "\n\n**** Test with local file sample_news.nt *****\n")
     (load-rdf-file "data/sample_news.nt")
     (let [results (query "select * { ?s ?p ?o } limit 5")]
       (pprint/pprint results)
@@ -12,12 +13,14 @@
 
 (deftest dbpedia-test
   (testing "Try SPARQL query to DBPedia endpoint"
+    (println "\n\n**** Test with DBPedia query *****\n")
     (pprint/pprint
       (query-dbpedia
         "select ?p where { <http://dbpedia.org/resource/Bill_Gates> ?p <http://dbpedia.org/resource/Microsoft> . } limit 10"))))
 
 (deftest wikidata-test
   (testing "Try SPARQL query to WikiData endpoint"
+    (println "\n\n**** Test with Wikidata query *****\n")
     (pprint/pprint
       (query-dbpedia
         "select * where { ?subject ?property ?object . } limit 10"))))
