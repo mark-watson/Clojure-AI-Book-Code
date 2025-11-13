111fa4016742c7d9f67136d3c0123644085a9cd4
Mark Watson <markw@markwatson.com>
2025-11-13 10:22:01 -0700

    -

--- litellm_api/test/litellm_api/core_test.clj
+++ litellm_api/test/litellm_api/core_test.clj
@@ -2,10 +2,28 @@
   (:require [clojure.test :refer :all]
             [litellm.router :as router]))
 
-(deftest completions-test
+;; :available-providers #{:gemini :openrouter :mistral :anthropic :openai :ollama}
+
+(deftest openai-completions-test
   (testing "OpenAI completions API with LiteLLM"
-    (router/quick-setup!)
-    (let [response (router/completion :openai
-                                      {:messages [{:role :user :content "please generate a 10 word sentence"}]})]
-      (println (router/extract-content response))
-      (is (not (nil? response))))))
+   (router/register!
+    :fast
+    {:provider :openai
+     :model "gpt-4o-mini"
+     :config {:api-key (System/getenv "OPENAI_API_KEY")}})
+   (let [response (router/completion :fast
+                   {:messages [{:role :user :content "please generate a 10 word sentence"}]})]
+     (println (router/extract-content response))
+     (is (not (nil? response))))))
+
+(deftest google-completions-test
+  (testing "Google Gemini completions API with LiteLLM"
+   (router/register!
+    :fast
+    {:provider :gemini
+     :model "gemini-2.5-flash"
+     :config {:api-key (System/getenv "GOOGLE_API_KEY")}})
+   (let [response (router/completion :fast
+                   {:messages [{:role :user :content "please generate a 10 word sentence"}]})]
+     (println (router/extract-content response))
+     (is (not (nil? response))))))
