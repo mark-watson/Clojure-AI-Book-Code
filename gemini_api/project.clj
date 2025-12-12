908db1cdbe08859b66a0b0e5b3f732e10f3a2682
Mark Watson <markw@markwatson.com>
2025-12-12 09:57:20 -0700

    code improvements by devstral-small-2

--- gemini_api/project.clj
+++ gemini_api/project.clj
@@ -1,10 +1,9 @@
-(defproject gemini_api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject gemini-api "0.1.0-SNAPSHOT"
+  :description "Gemini API client for Clojure"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [clj-http "3.12.1"]
-                 [org.clojure/data.json "2.5.0"]
-                 ]
-  :repl-options {:init-ns gemini-api.core})
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/tools.logging "1.2.4"]]  ; Add this line
+  :repl-options {:init-ns gemini-api.core}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]
+                   :plugins [[lein-cljfmt "0.8.0"]]}})
--- gemini_api/src/gemini_api/core.clj
+++ gemini_api/src/gemini_api/core.clj
@@ -1,23 +1,31 @@
 (ns gemini-api.core
-  (:require [clj-http.client :as client])
-  (:require [clojure.data.json :as json]))
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clojure.tools.logging :as log]))
 
 (def model "gemini-2.5-flash") ; or gemini-3.0-pro, etc.
 
-(def google-api-key (System/getenv "GOOGLE_API_KEY")) ; Make sure to set this env variable
+(def google-api-key (System/getenv "GOOGLE_API_KEY"))
+(when (nil? google-api-key)
+  (log/error "GOOGLE_API_KEY environment variable not set!"))
 
 (def base-url "https://generativelanguage.googleapis.com/v1beta/models")
 
-(defn generate-content [prompt]
-  (let [url (str base-url "/" model ":generateContent?key=" google-api-key)
+(defn build-request-url [model api-key]
+  (str base-url "/" model ":generateContent?key=" api-key))
+
+(defn generate-content [prompt & {:keys [model api-key] :or {model model api-key google-api-key}}]
+  (let [url (build-request-url model api-key)
         headers {"Content-Type" "application/json"}
-        body {:contents [{:parts [{:text prompt}]}]}]
+        body {:contents [{:parts [{:text prompt}]}]}
+        request-opts {:headers headers
+                      :body (json/write-str body)
+                      :content-type :json
+                      :accept :json
+                      :socket-timeout 10000
+                      :connection-timeout 10000}]
     (try
-      (let [response (client/post url {:headers headers
-                                     :body (json/write-str body)
-                                     :content-type :json
-                                     :accept :json})
-            _ (println "Raw response:" (:body response))  ; Debug print
+      (let [response (client/post url request-opts)
             parsed-response (json/read-str (:body response) :key-fn keyword)
             candidates (:candidates parsed-response)]
         (if (seq candidates)
@@ -25,16 +33,20 @@
             (if text
               text
               (do
-                (println "No text found in response structure:" parsed-response)
+                (log/warn "No text found in response structure:" parsed-response)
                 nil)))
           (do
-            (println "No candidates found in response:" parsed-response)
+            (log/warn "No candidates found in response:" parsed-response)
             nil)))
       (catch Exception e
-           (println "Error making request:" (.getMessage e))
-           (when-let [response-body (-> e ex-data :body)]
-             (println "Error response body:" response-body))
-           nil))))
+        (log/error "Error making request:" (.getMessage e))
+        (when-let [response-body (-> e ex-data :body)]
+          (log/error "Error response body:" response-body))
+        nil))))
 
 (defn summarize [text]
   (generate-content (str "Summarize the following text:\n\n" text)))
+
+;; Example usage:
+;; (generate-content "Explain quantum computing")
+;; (summarize "Long text to summarize...")
